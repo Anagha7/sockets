c7a4a0905f6a205c058f89807cfb30cb9488195e
Anagha7 <46987154+Anagha7@users.noreply.github.com>
2019-02-07 16:08:35 +0530

    Add files via upload

---
+++ w.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Hello there!',
+                              from_='whatsapp:+14155238886',
+                              to='whatsapp:+15005550006'
+                          )
+
+print(message.sid)
